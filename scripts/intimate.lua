bf5485afa195c43e2c519e873a246fd2d3ddf5f0
akamediasystem <nfeehan@gmail.com>
2024-12-14 15:38:57 -0500

    adding estop-tester, which seems successfully to actuvate estop and re-start it with minimal delay in spinning up motors

--- scripts/intimate.lua
+++ scripts/intimate.lua
@@ -26,20 +26,21 @@ mavlink:init(1, 10)
 local tseverity = 2  -- "Critical" level
 currentIndex = 1
 
--- Function to check if the quadcopter is armed and above 3 meters
-function is_above_3m_and_armed()
-    return 1
-end
-
 function send_status_text()
     local textArray = {
     "YOU AND I ARE HERE",
     "YOU AND I ARE HERE",
-    "  ",
-    "  ",
-    "  ",
-    "  ",
-    "  ",
+    " o ",
+    " o ",
+    " o ",
+    " o ",
+    " o ",
+    " o ",
+    " o o ",
+    " o o ",
+    " o o ",
+    " o o o ",
+    "YOU AND I ARE HERE",
     "UP HERE TREADING WATER",
     "YOUR EYES LOCKED ON",
     "HERE TO COMMUNICATE",
@@ -90,42 +91,35 @@ function send_status_text()
     "THE LOOP WILL END",
     "I WILL FALL I WILL FALL I WILL FALL I WILL FALL I",
     "IWILLFALLIWILLFALLIWILLFALLIWILLFALLIWILLFALLIWILL",
-    "BUT NOT YET"
+    "BUT NOT YET",
+    " x x x ",
+    " x x x ",
+    " x x x ",
+    " x x x ",
+    " x x x "
     }
 
     -- Ensure currentIndex is within bounds
     if currentIndex > #textArray or currentIndex < 1 then
-        gcs:send_text(2, "currentIndex out of bounds: " .. tostring(currentIndex))
+        -- gcs:send_text(2, "currentIndex out of bounds: " .. tostring(currentIndex))
         currentIndex = 1  -- Reset to 1 if out of bounds
     end
 
-    if 1 then
-        local the_text = textArray[currentIndex]
-        local truncated_text = the_text:sub(1, 50)
-        -- gcs:send_text(2, truncated_text)
-        local result_mission_planner = mavlink:send_chan(0, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text =truncated_text}))
-        local result_radio_telem = mavlink:send_chan(1, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text =truncated_text}))
-        -- local result_mission_planner = mavlink:send_chan(0, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text=truncated_text}))
-        local result_osd = mavlink:send_chan(2, mavlink_msgs.encode("STATUSTEXT", {severity= tseverity, text=truncated_text}))
+    local the_text = textArray[currentIndex]
 
-        -- Increment currentIndex for the next run
-        currentIndex = currentIndex + 1
-        if currentIndex > #textArray then
-            -- currentIndex = 1  -- Loop back to the first message
-            return
-        end
-    else
-        local result_mission_planner = mavlink:send_chan(0, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text ="AWAITING PERFECT TIME"}))
-        local result_radio_telem = mavlink:send_chan(1, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text ="AWAITING PERFECT TIME"}))
-        -- local result_mission_planner = mavlink:send_chan(0, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text="AWAITING PERFECT TIME"}))
-        local result_osd = mavlink:send_chan(2, mavlink_msgs.encode("STATUSTEXT", {severity= tseverity, text="AWAITING PERFECT TIME"}))
-    end
-    -- return send_status_text, 7000
-    -- return
+    local truncated_text = the_text:sub(1, 50)
+    -- gcs:send_text(2, truncated_text)
+    local result_mission_planner = mavlink:send_chan(0, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text =truncated_text}))
+    local result_radio_telem = mavlink:send_chan(1, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text =truncated_text}))
+    -- local result_mission_planner = mavlink:send_chan(0, mavlink_msgs.encode("STATUSTEXT", {severity = tseverity, text=truncated_text}))
+    local result_osd = mavlink:send_chan(2, mavlink_msgs.encode("STATUSTEXT", {severity= tseverity, text=truncated_text}))
+
+    -- Increment currentIndex for the next run
+    currentIndex = currentIndex + 1
+    -- if currentIndex > #textArray then
+    --     currentIndex = 1  -- Loop back to the first message
+    -- end
+    return send_status_text, 7000
 end
 
--- Example of calling the function in the main loop
-function update()
-    send_status_text()
-    return update, 7000
-end
+return send_status_text()
